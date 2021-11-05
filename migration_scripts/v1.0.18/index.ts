bd16638aa5ee24ec77d615a811a328ed53bdfc7f
Kevin Giordani <giordani.kevin.02@gmail.com>
2021-11-05 10:39:19 +0100

    index for scripts

--- migration_scripts/v1.0.18/index.ts
+++ migration_scripts/v1.0.18/index.ts
@@ -1,14 +1,24 @@
-import { denormalizeWorldsAndOrigins } from './denormalizeWorldsAndOrigins';
+import { addDefaultTemplateContent } from '../v1.0.x/scripts/addDefaultTemplateContent';
+import { addActionsHash } from './addActionsHash/index';
+import { createAllProspectReplies } from './createAllProspectReplies/index';
+import { createConnectRepliedStats } from './createConnectRepliedStats/index';
+import { retrieveActionStatsProspects } from './retrieveActionStatsProspects/index';
 
 const maintenanceRequired = async () => {
     console.log('Debut');
     console.time('Script for maintenance');
 
-    await denormalizeWorldsAndOrigins();
+    // await denormalizeWorldsAndOrigins();
+    await addActionsHash();
+    await addDefaultTemplateContent();
+    await retrieveActionStatsProspects();
 
     console.timeEnd('Script for maintenance');
 };
 
-const nonMaintenanceRequired = async () => {};
+const nonMaintenanceRequired = async () => {
+    await createConnectRepliedStats();
+    await createAllProspectReplies();
+};
 
 maintenanceRequired();
